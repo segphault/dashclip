600b3af5ef3e1add21bc807d170d73821577dd3b
Ryan Paul <seg@phault.net>
2016-06-21 13:06:05 -0700

    Add project files.

---
+++ DashClip/ActiveWindow.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace DashClip
+{
+    public class ActiveWindow
+    {
+        public delegate bool EnumWindowsProc(IntPtr hwnd, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        public static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, uint dwExtraInfo);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+        [DllImport("user32")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool EnumChildWindows(IntPtr window, EnumWindowsProc callback, IntPtr i);
+
+        private static bool EnumWindow(IntPtr handle, IntPtr pointer) {
+            GCHandle gch = GCHandle.FromIntPtr(pointer);
+            List<IntPtr> list = gch.Target as List<IntPtr>;
+
+            if (list == null)
+                throw new InvalidCastException("GCHandle Target could not be cast as List<IntPtr>");
+
+            list.Add(handle);
+            return true;
+        }
+
+        public static List<IntPtr> GetChildWindows(IntPtr parent) {
+            List<IntPtr> result = new List<IntPtr>();
+            GCHandle listHandle = GCHandle.Alloc(result);
+
+            try {
+                EnumWindowsProc childProc = new EnumWindowsProc(EnumWindow);
+                EnumChildWindows(parent, childProc, GCHandle.ToIntPtr(listHandle));
+            }
+            finally {
+                if (listHandle.IsAllocated)
+                    listHandle.Free();
+            }
+
+            return result;
+        }
+
+        Process windowProcess;
+        
+        public ActiveWindow() {
+            var window = GetForegroundWindow();
+            windowProcess = GetWindowProcess(window);
+
+            if (windowProcess.ProcessName == "ApplicationFrameHost")
+                windowProcess = GetWindowProcess(GetChildWindows(window)[0]);
+        }
+
+        public Process GetWindowProcess(IntPtr w) {
+            uint procId;
+            GetWindowThreadProcessId(w, out procId);
+            return Process.GetProcessById((int)procId);
+        }
+
+        public string GetAppName() {
+            if (windowProcess == null) return null;
+            return windowProcess.MainModule.FileVersionInfo.ProductName;
+        }
+
+        public Icon GetAppIcon() {
+            return windowProcess != null ?
+                   Icon.ExtractAssociatedIcon(windowProcess.MainModule.FileName) : null;
+        }
+
+        public static void Paste() {
+            keybd_event((byte)0x11, 0, 0, 0);
+            keybd_event((byte)0x56, 0, 0, 0);
+
+            keybd_event((byte)0x11, 0, (byte)0x0002, 0);
+            keybd_event((byte)0x56, 0, (byte)0x0002, 0);
+        }
+    }
+}
---
+++ DashClip/ClipboardMonitor.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Interop;
+
+namespace DashClip
+{
+    public sealed class ClipboardMonitor : IDisposable
+    {
+        // Taken from here:
+        // http://codereview.stackexchange.com/questions/115417/monitoring-the-clipboard
+
+        private static class NativeMethods
+        {
+            [DllImport("user32.dll", SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            public static extern bool AddClipboardFormatListener(IntPtr hwnd);
+
+
+            [DllImport("user32.dll", SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            public static extern bool RemoveClipboardFormatListener(IntPtr hwnd);
+
+            public const int WM_CLIPBOARDUPDATE = 0x031D;
+            public static IntPtr HWND_MESSAGE = new IntPtr(-3);
+        }
+
+        private HwndSource hwndSource = new HwndSource(0, 0, 0, 0, 0, 0, 0, null, NativeMethods.HWND_MESSAGE);
+        private bool disposed = false;
+
+        public ClipboardMonitor()
+        {
+            hwndSource.AddHook(WndProc);
+            NativeMethods.AddClipboardFormatListener(hwndSource.Handle);
+        }
+
+        public void Dispose()
+        {
+            if (disposed) return;
+
+            NativeMethods.RemoveClipboardFormatListener(hwndSource.Handle);
+            hwndSource.RemoveHook(WndProc);
+            hwndSource.Dispose();
+            disposed = true;
+        }
+
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            if (msg == NativeMethods.WM_CLIPBOARDUPDATE)
+                ClipboardContentChanged?.Invoke(this, EventArgs.Empty);
+
+            return IntPtr.Zero;
+        }
+
+        public event EventHandler ClipboardContentChanged;
+    }
+}
